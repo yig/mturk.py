d55d5a2c2e7bc92d7740b3ad87ecc336fcefb978
Yotam Gingold <yotam@yotamgingold.com>
2013-04-26 00:09:54 -0400

    better wording for qualifications help text

--- mturk.py
+++ mturk.py
@@ -492,7 +492,7 @@ def main():
 }'''
         
         print >> sys.stderr, 'Note: Commands run in the sandbox unless "really" is present.'
-        print >> sys.stderr, 'Note: The "qualifications" entry can be omitted.  The default is to have no qualifications.  Any qualification type supported by boto is allowed.'
+        print >> sys.stderr, 'Note: The "qualifications" field is optional.  The default is to have no qualifications.  Any qualification type supported by boto is allowed.'
         
         sys.exit(-1)
     
